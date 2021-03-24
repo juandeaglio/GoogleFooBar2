89da6815c06ef96bdb931dfbf040495cff168805
juandeaglio <45726108+juandeaglio@users.noreply.github.com>
2021-03-24 15:05:49 -0700

    Refactored test to include correct assertion.

--- src/UnitTests.java
+++ src/UnitTests.java
@@ -11,6 +11,14 @@ public class UnitTests
         int[] pegPositions = {1,2};
         int[] solutionExpected = {-1,-1};
         AxialGearConfigurator configurator = new AxialGearConfigurator();
-        Assertions.assertTrue(Arrays.equals(configurator.FindAxialFirstGear(pegPositions),solutionExpected));
+        Assertions.assertArrayEquals(configurator.FindAxialFirstGear(pegPositions),solutionExpected);
+    }
+    @Test
+    void GivenThreePegsShouldReturnCorrectResult()
+    {
+        int[] pegPositions = {4,30,50};
+        int[] solutionExpected = {12,1};
+        AxialGearConfigurator configurator = new AxialGearConfigurator();
+        Assertions.assertArrayEquals(configurator.FindAxialFirstGear(pegPositions),solutionExpected);
     }
 }
