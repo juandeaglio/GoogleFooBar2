fde334ff9e0a3e5277c89499d2517635f9433ac4
juandeaglio <45726108+juandeaglio@users.noreply.github.com>
2021-03-24 20:34:49 -0700

    Huge addition, in form of RREF may need to revert changes if it does not work.

--- src/AxialGearConfigurator.java
+++ src/AxialGearConfigurator.java
@@ -1,12 +1,159 @@
 public class AxialGearConfigurator
 {
+    public static int[] solution(int [] pegs)
+    {
+        AxialGearConfigurator gearConfigurator = new AxialGearConfigurator();
+        return gearConfigurator.FindAxialFirstGear(pegs);
+    }
    public int[] FindAxialFirstGear(int[] pegPositions)
    {
        int[] result = {-1,-1};
        int[][]systemOfEquations = createSystemOfEquations(pegPositions);
-
+       systemOfEquations = convertToReducedRowEchelonForm(systemOfEquations);
        return result;
    }
+
+    private int[][] convertToReducedRowEchelonForm(int[][] systemOfEquations)
+    {
+        int[] pivot = new int[2];
+        int current = 0;
+        for(int i = 0; i < systemOfEquations[0].length; i++)
+        {
+            pivot[1] = i;
+            //Step 1
+            for(int j = i; j < systemOfEquations[0].length; j++)
+            {
+                if(IsColumnZeroes(systemOfEquations, pivot))
+                    break;
+                else
+                    pivot[1] = j;
+            }
+            //Step 2
+            int[] newPivot = {pivot[0], pivot[1]};
+            int initialRow = pivot[0];
+            pivot = FindPivot(systemOfEquations, pivot, newPivot, systemOfEquations.length - initialRow);
+
+            if(systemOfEquations[pivot[0]][pivot[1]] == 0)
+            {
+                pivot[0]++;
+                continue;
+            }
+
+            if(pivot[0] != current)
+            {
+                current = SwapToTop(systemOfEquations, pivot, current);
+            }
+
+            if(systemOfEquations[pivot[0]][pivot[1]] != 1)
+            {
+                ScaleRow(systemOfEquations, pivot);
+            }
+            //Step 3
+            SetZeroBelowPivot(systemOfEquations, pivot);
+            //Step 5
+            SetNumbersLeftOfLeadingOneToZero(systemOfEquations, pivot);
+            //Step 4
+            if(pivot[0]+1 >= systemOfEquations.length)
+            {
+                break;
+            }
+            boolean skip = false;
+            for(int k = 0; k < systemOfEquations[0].length; k++)
+            {
+                if(systemOfEquations[pivot[0]+1][k] != 0)
+                {
+                    skip = true;
+                    break;
+                }
+            }
+            if(skip)
+                break;
+            current++;
+            pivot[0]++;
+        }
+        return systemOfEquations;
+    }
+
+    private void SetNumbersLeftOfLeadingOneToZero(int[][] systemOfEquations, int[] pivot) {
+        for(int k = pivot[0]; k >= 0; k--)
+        {
+            if(k == pivot[0])
+            {
+                if (systemOfEquations[pivot[0]][pivot[1]] != 1)
+                    ScaleRow(systemOfEquations, pivot);
+                continue;
+            }
+
+            int[] abovePivot = {k, pivot[1]};
+            int complement = -systemOfEquations[abovePivot[0]][abovePivot[1]] / systemOfEquations[pivot[0]][pivot[1]];
+            AddToAnotherRowMultipliedByOriginalPivot(systemOfEquations[0], pivot, abovePivot, complement);
+        }
+    }
+
+    private int SwapToTop(int[][] systemOfEquations, int[] pivot, int current) {
+        int[] temp = systemOfEquations[current];
+        systemOfEquations[current] = systemOfEquations[pivot[0]];
+        systemOfEquations[pivot[0]] = temp;
+        int t = current;
+        current = pivot[0];
+        pivot[0] = t;
+        return current;
+    }
+
+    private int[] FindPivot(int[][] systemOfEquations, int[] pivot, int[] newPivot, int i1) {
+        for(int k = pivot[0]; k < i1; k++)
+        {
+            pivot[0] = k;
+            if(systemOfEquations[pivot[0]][pivot[1]] != 0)
+            {
+                newPivot[0] = k;
+                break;
+            }
+        }
+        pivot = newPivot;
+        return pivot;
+    }
+
+    private boolean IsColumnZeroes(int[][] systemOfEquations, int[] pivot)
+    {
+        for(int k = 0; k < systemOfEquations.length; k++)
+        {
+            if( systemOfEquations[k][pivot[1]] != 0)
+                return false;
+        }
+        return true;
+
+    }
+
+    private void SetZeroBelowPivot(int[][] systemOfEquations, int[] pivot) {
+        for(int k = pivot[0]; k < systemOfEquations.length; k++)
+        {
+            if(k == pivot[0])
+                continue;
+            int[] belowPivot = {k, pivot[1]};
+            int complement = -systemOfEquations[belowPivot[0]][belowPivot[1]] / systemOfEquations[pivot[0]][pivot[1]];
+            AddToAnotherRowMultipliedByOriginalPivot(systemOfEquations[0], pivot, belowPivot, complement);
+        }
+    }
+
+    private void AddToAnotherRowMultipliedByOriginalPivot(int[] systemOfEquation, int[] pivot, int[] belowPivot, int complement)
+    {
+        for(int l = 0; l < systemOfEquation.length; l++)
+            belowPivot[l] += pivot[l]*complement;
+    }
+
+    private void ScaleRow(int[][] systemOfEquations, int[] pivot)
+    {
+        int divider = systemOfEquations[pivot[0]][pivot[1]];
+        for(int k = 0; k < systemOfEquations[0].length; k++)
+        {
+            int[] temp = systemOfEquations[pivot[0]];
+            for(int l = 0; l < temp.length; l++)
+            {
+               temp[l] /= divider;
+            }
+        }
+    }
 
     private int[][] createSystemOfEquations(int[] pegPositions)
     {
